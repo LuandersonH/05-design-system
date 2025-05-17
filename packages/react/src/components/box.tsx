caa39ad166f07f1515eca58122108a04a26976b9
LuandersonH <work.luanderson@gmail.com>
2025-05-17 00:38:36 -0300

    Componente: Box

---
+++ packages/react/src/components/box.tsx
@@ -0,0 +1,18 @@
+import { styled } from '../styles'
+import type { VariantProps } from '@stitches/react'
+
+export const Box = styled('div', {
+  padding: '$4',
+  borderRadius: '$md',
+  backgroundColor: '$gray800',
+  border: '1px solid $gray600',
+
+  //   width: '100%',
+  //   maxWidth: ''
+})
+
+export type BoxProps = VariantProps<typeof Box>
+
+// Para aparecer td:
+// import { ComponentProps } from 'react'
+// export interface BoxProps extends ComponentProps<typeof Box> {}
