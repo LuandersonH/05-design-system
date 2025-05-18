cb0bd55735f6dbee994012c3a0bf6727c9834a5f
LuandersonH <work.luanderson@gmail.com>
2025-05-18 17:05:28 -0300

    Componente: Avatar (Red Hat desativado, gerando dependencia no package.json)

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+import { Meta, StoryObj } from '@storybook/react/*'
+
+export default {
+  title: 'Data Display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://gru.ifsp.edu.br/images/phocagallery/galeria2/image03_grd.png',
+    alt: 'Avatar',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
