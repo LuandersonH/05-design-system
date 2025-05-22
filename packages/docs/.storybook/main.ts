1eefccb23809dbc80c00fc678fcc2842b1af23c6
LuandersonH <work.luanderson@gmail.com>
2025-05-22 12:41:49 -0300

    ci: Add docs workflow

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -1,19 +1,32 @@
 import type { StorybookConfig } from '@storybook/react-vite';
- 
+import { mergeConfig } from 'vite';
+
 const config: StorybookConfig = {
   framework: '@storybook/react-vite',
   stories: [
-    "../src/pages/*.mdx", 
-    "../src/pages/**/*.mdx", 
-    "../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    '../src/pages/*.mdx',
+    '../src/pages/**/*.mdx',
+    '../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
   addons: [
     '@storybook/addon-essentials',
-    '@storybook/addon-a11y'
+    '@storybook/addon-a11y',
   ],
-    docs: {
-    autodocs: true, 
+  docs: {
+    autodocs: true,
+  },
+  async viteFinal(config, { configType }) {
+    if (configType === 'PRODUCTION') {
+      return mergeConfig(config, {
+        base: '/05-design-system/',
+        build: {
+          manifest: true,
+          assetsDir: 'assets',
+        },
+      });
+    }
+    return config;
   },
 };
- 
+
 export default config;
--- packages/docs/vite.config.js
+++ packages/docs/vite.config.js
@@ -3,7 +3,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/05-design-system/',
+  build: {
+    manifest: true,
+    assetsDir: 'assets',
+  },
 })
